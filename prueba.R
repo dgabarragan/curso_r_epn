23d5d52781d4711e5fa904bb0225cc42b9853220
dgabarragan <day_irie@hotmail.com>
2015-10-15 20:25:24 -0500

    tipos de vector

--- prueba.R
+++ prueba.R
@@ -1,3 +1,34 @@
-2+2
-library(brew)
-mean(x)
+x<-4
+typeof(vector)
+lenght(vector)
+vector<-c(4,5,6)
+lenght(vector)
+attributes(x)
+
+x<-vector ("numeric", 6)
+y<-c(1,2,3,4,5,6,10,30)
+x+y
+
+n<-length(x)
+x[c(n,n-1)]
+vec
+
+
+
+vec <-seq(1,100,by=0.16)
+vec
+length(vec)
+
+vec[c(99,360)]
+
+
+si<-c(F,T,F,T,T)
+
+si[c(2,4)]
+
+typeof(si)
+
+is.numeric(si)
+
+typeof(si)
+
